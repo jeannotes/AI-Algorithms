f6cff924249bcdc03cefe0ab38ab4ecffd422d4f
jean <j.z.feng@foxmail.com>
2018-08-17 10:57:48 +0800

    graph_based/

---
+++ pattern_recognization/segment/graph_based/graph-based-image-segmentation/build/cli/CMakeFiles/refh_cli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/refh_cli.dir/main.cpp.o"
+  "../../bin/refh_cli.pdb"
+  "../../bin/refh_cli"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/refh_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pattern_recognization/segment/graph_based/graph-based-image-segmentation/build/lib/CMakeFiles/refh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/refh.dir/graph_segmentation.cpp.o"
+  "librefh.pdb"
+  "librefh.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/refh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
